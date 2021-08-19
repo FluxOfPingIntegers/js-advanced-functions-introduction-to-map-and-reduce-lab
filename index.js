07a9ae74c6f03266fb93fbb502fedb4cd98197a8
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-08-18 23:22:24 -0400

    commit tree 934ab46135b35f239d64960634a43ea708696cc8

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+
+// map-like
+function mapToNegativize(sourceArray) {
+  console.log(`sourceArray: ${sourceArray}`);
+  return sourceArray.map(element => element * -1);
+}
+
+function mapToNoChange(sourceArray) {
+  console.log(`sourceArray: ${sourceArray}`);
+  return sourceArray.map(element => element);
+}
+
+function mapToDouble(sourceArray) {
+  console.log(`sourceArray: ${sourceArray}`);
+  return sourceArray.map(element => element * 2);
+}
+
+function mapToSquare(sourceArray) {
+  console.log(`sourceArray: ${sourceArray}`);
+  return sourceArray.map(element => element * element);
+}
+// reduce-like
+function reduceToTotal(sourceArray, startingPoint) {
+  let reducer = (accumulator, currentValue) => accumulator + currentValue;
+  return (!!startingPoint ? sourceArray.reduce(reducer, startingPoint) : sourceArray.reduce(reducer))
+}
+
+function reduceToAllTrue(sourceArray) {
+  let isTrue = (element) => !!element
+  return sourceArray.every(isTrue);
+}
+
+function reduceToAnyTrue(sourceArray) {
+  let isTrue = (element) => element == true
+  return sourceArray.some(isTrue);
+}
